b11beee4b497aef07697baca5adb282d7b338653
kato1988 <pgmjpgmn@gmail.com>
2020-02-02 13:52:03 +0900

    add test scene

---
+++ ggjbs2020_blinds/Assets/Scripts/Clear.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clear : MonoBehaviour
+ { 
+    public cleartext clearText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
