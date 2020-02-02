0d839ec0c87fab9257802d08e97a4e9797a6ad8a
kato1988 <pgmjpgmn@gmail.com>
2020-02-02 15:26:17 +0900

    add congraturation

--- ggjbs2020_blinds/Assets/Scripts/Clear.cs
+++ ggjbs2020_blinds/Assets/Scripts/Clear.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Clear : MonoBehaviour
- { 
-    public cleartext clearText;
+{
+    Text text;
+    float speed = 0.01f;
+    float alfa;
+    float red, green, blue;
+    bool clearflag;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //  this.text = this.GetComponent<Text>();
+        text = GetComponent<Text>();
+        red = this.GetComponent<Text>().color.r;
+        green = this.GetComponent<Text>().color.g;
+        blue = this.GetComponent<Text>().color.b;
+        alfa = this.GetComponent<Text>().color.a;
+        clearflag = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (clearflag == true)
+        {
+            GetComponent<Text>().color = new Color(red, green, blue, alfa);
+            alfa += speed;
+        }
+        text.color = new Color(red, green, blue, alfa);
     }
 }
