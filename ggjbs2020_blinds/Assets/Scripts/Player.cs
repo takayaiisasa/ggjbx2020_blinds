38fdb99573308533432beeb25a9d7f63ef5bdd54
takayaiisasa <tky-0414.matu6baske@ezweb.ne.jp>
2020-02-02 10:10:24 +0900

    [add] Door, Player.Sc

---
+++ ggjbs2020_blinds/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static bool start = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (start)
+        {
+            float move;
+            move = 0.3f * Time.deltaTime;
+            transform.position += new Vector3(0, 0, move);
+        }
+    }
+}
