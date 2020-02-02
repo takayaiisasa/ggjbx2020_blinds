bac44ed0a994f26d7f23b7c9dd887d74f2c18745
kato1988 <pgmjpgmn@gmail.com>
2020-02-02 10:01:27 +0900

    音鳴らす処理追加

---
+++ ggjbs2020_blinds/Assets/Scripts/HitPipe.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPipe : MonoBehaviour
+{
+    private AudioSource CatchPipe;
+    private AudioSource PipeNear;
+    private AudioSource PipeFar;
+    private bool TrrigerFlag;
+    private float Timmer;
+
+    void Start()
+    {
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        CatchPipe = audioSources[0];
+        PipeNear = audioSources[1];
+        PipeFar = audioSources[2];
+        TrrigerFlag = false;
+        Timmer = 0;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (TrrigerFlag == true && timer >= 2)
+        {
+            TrrigerFlag = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)  // ぶつかったオブジェクトのタグで分岐
+        {
+            case "goal":
+                {
+                    CatchPipe.PlayOneShot(CatchPipe.clip);
+                    TriggerFlag = true;
+                    Timer = 0;
+                    break;
+                }
+            case "close":
+                {
+                    PipeNear.PlayOneShot(PipeNear.clip);
+                    TriggerFlag = true;
+                    Timer = 0;
+                    break;
+                }
+            case "far":
+                {
+                    PipeFar.PlayOneShot(PipeFar.clip);
+                    TriggerFlag = true;
+                    Timer = 0;
+                    break;
+                }
+        }
+    }
+}
